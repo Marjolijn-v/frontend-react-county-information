3e878921eeec507708af76ce70183769988e5530
Marjolijn-v <mverspiek@gmail.com>
2025-12-08 12:39:11 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css';
+import axios from 'axios';
+import world_map from './assets/world_map.png'
+
 
 function App() {
 
+    async function fetchCountries () {
+        try {
+            const result = await axios.get('https://restcountries.com/v3.1/all?fields=name');
+            console.log(result.data);
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    fetchCountries();
+
+
+
     return (
         <>
-            Maak je applicatie hier!
+            <header>
+                <img className="header-image" src={world_map} alt="map of the world"/>
+            </header>
+            <button>
+
+            </button>
+
         </>
     )
 }
